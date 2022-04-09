040403aec25fbac840ee83590b2233c5787b78d0
ettolrach <ettolrach@disroot.org>
2022-04-09 23:03:07 +0100

    Finish writing move checking functions

--- app/Board.hs
+++ app/Board.hs
@@ -12,6 +12,11 @@ type Grid = [Space]
 type Coordinate = (Int, Int)
 type Direction = (Int, Int)
 
+data Game = Game
+  { board :: Grid
+  , width :: Int
+  , activePlayer :: Colour }
+
 flip :: Colour -> Colour
 flip Black = White
 flip White = Black
@@ -29,17 +34,36 @@ emptyGrid :: Int -> Grid
 emptyGrid 0 = []
 emptyGrid n = Empty : emptyGrid (n-1)
 
+startingGrid :: Grid
+startingGrid =
+  [ Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty
+  , Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty
+  , Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty
+  , Empty, Empty, Empty, Counter Black, Counter White, Empty, Empty, Empty
+  , Empty, Empty, Empty, Counter White, Counter Black, Empty, Empty, Empty
+  , Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty
+  , Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty
+  , Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty ]
+
 directions :: [Coordinate]
 directions = [(0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1), (-1, 0), (-1,-1)]
 
-getIndiciesToFlip :: Grid -> Int -> Coordinate -> Colour -> [Coordinate]
-getIndiciesToFlip g width (x,y) c = concat [inDirection g width (x,y) c (dx,dy) | (dx, dy) <- directions]
+getIndiciesToFlip :: Game -> Coordinate -> [Coordinate]
+getIndiciesToFlip Game { board = g, width = w, activePlayer = c } (x,y) = concat [inDirection g w (x,y) c (dx,dy) | (dx, dy) <- directions]
   where
     inDirection :: Grid -> Int -> Coordinate -> Colour -> Direction -> [Coordinate]
     inDirection g width (x,y) c (dx,dy)
+      -- If the clicked coordinate isn't empty, it's an illegal move.
+      | spaceAtIndex g (x, y) width /= Empty = []
       -- If it's out of bounds.
-      | x + dx > width || y + dy > width = []
+      | x + dx >= width || y + dy >= width || x + dx < 0 || y + dy < 0 = []
       -- If the colour of the counter isn't the desired one.
       | spaceAtIndex g (x + dx, y + dy) width /= Counter c = []
       -- Otherwise, it's a valid coordinate and the function should keep checking the next one.
       | otherwise = (x + dx, y + dy) : inDirection g width (x + dx, y + dy) c (dx,dy)
+
+legal :: Game -> Coordinate -> Bool
+legal game coord = getIndiciesToFlip game coord /= []
+
+movesAvailable :: Game -> Bool
+movesAvailable Game { board = g, width = w, activePlayer = c } = or [legal (Game g w c) (x, y) | y <- [0..w], x <- [0..w]]
